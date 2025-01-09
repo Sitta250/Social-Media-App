836efa13c3c10c7a73387ba033f5e0f816c03a4c
Ben <sitta@gmail.com>
2025-01-09 01:31:37 +0100

    Initial commit: Added Social Media App with Firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD32YsZeH1-Sj-bD84w2x8tDb7W9_9B9wc",
-  authDomain: "react-socialmedia-ce04f.firebaseapp.com",
-  projectId: "react-socialmedia-ce04f",
-  storageBucket: "react-socialmedia-ce04f.appspot.com",
-  messagingSenderId: "453089117412",
-  appId: "1:453089117412:web:31023c861290ed885858f1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
